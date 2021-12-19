334223ca2d43cccf643aaee431e4b4b1fd8fd0a7
Bennet Wilson <bennetwilson@gmail.com>
2021-12-19 14:50:42 -0700

    First commit. Restarted twice already

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var apiKey = "99ba66799456699dd49ae0c3361c4eba";
+
+// var searchQueryURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+ apiKey;
+
+
+
+
+// var weatherApi = api.openweathermap.org/data/2.5/forecast?q={city name}&appid={99ba66799456699dd49ae0c3361c4eba}
