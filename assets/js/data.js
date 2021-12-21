89167bc22f4ba3afd03ee42a6a37fedbde50c7d1
Bennet Wilson <bennetwilson@gmail.com>
2021-12-20 21:57:06 -0700

    Worked with tutor. Fixed functionality. Got UV and 5 day to work

--- assets/js/data.js
+++ assets/js/data.js
@@ -5,11 +5,17 @@ const search = document.getElementById("userSearch");
 const button = document.getElementById("submit");
 
 
-button.addEventListener("click", () => {
+button.addEventListener("click", async () => {
     const currentVal = search.value;
 
-    ft.getCurrent(currentVal).then((data) => {
-        ui.populateUI(data);
+        const data = await ft.getCurrent(currentVal)
+      
+        const oneCallData = await ft.getOneCall(data);
+   
+        data.uvi = oneCallData.current.uvi
+        
+        await ui.populateUI(data);
+       await ui.populateCards(oneCallData)
         // ui.saveToLs(data);
-    });
+    
 });
--- assets/js/ui.js
+++ assets/js/ui.js
@@ -2,29 +2,46 @@
 class Ui {
     constructor() {
         this.uiContainer = document.getElementById("content");
+
+        this.uiFiveday= document.getElementById("fiveday");
         this.city;
     }
     populateUI(data) {
-        this.uiContainer.innerHTML = `
+        this.uiContainer.innerHTML =       this.uiContainer.innerHTML+`
         <div class="card mx-auto mt-5" style="width: 18rem;">
             <div class="card-body justify-content-center">
                 <h5 class="card-title">${data.name}</h5>
-                <h6 class="card-subtitle mb-2 text-muted">Currently, it's ${data.main.temp}F.<br> Highs of ${data.main.temp_max}F.<br> Lows of ${data.main.temp_min}F.<br> Humidity is ${data.main.humidity}%.<br> Wind speed of ${data.wind.speed} miles per hour.</h6>
+                <h6 class="card-subtitle mb-2 text-muted">Currently, it's ${data.main.temp}F.<br> Highs of ${data.main.temp_max}F.<br> Lows of ${data.main.temp_min}F.<br> Humidity is ${data.main.humidity}%.<br> Wind speed of ${data.wind.speed} MPH.<br> Uv is ${data.uvi}</h6>
                 <p class="card-text"> Weather conditions are described as: ${data.weather[0].description}</p>
             </div>
         </div>
+        
+      
         `;
     }
+
+    populateCards(oneCallData) {
+        this.uiFiveday.innerHTML =  this.uiFiveday.innerHTML +`
+        
+        `
+        for (let i = 1; i < 6; i++) {
+            
+            this.uiFiveday.innerHTML =  this.uiFiveday.innerHTML +`
+            <div class="card mx-auto mt-5" style="width: 18rem;">
+            <div class="card-body justify-content-center">
+                <h5 class="card-title">${moment(oneCallData.daily[i].dt,"X").format("MM/DD/YYYY") }</h5>
+                <h6 class="card-subtitle mb-2 text-muted">High of ${oneCallData.daily[i].temp.max}. <br> Lows of ${oneCallData.daily[i].temp.min}.<br> Humidity is ${oneCallData.daily[i].humidity}%.<br> Wind speed of ${oneCallData.daily[i].wind_speed} MPH.<br> UVI is ${oneCallData.daily[i].uvi}.</h6>
+                <p class="card-text"></p>
+            </div>
+            </div>
+            `
+        }
+      
+      
+    }
     // populateUI(data) {`
-    // <div class="row justify-content-center">
-    // <div id="5dayForecast" class = "card-deck position-relative ">
-    //     <div class="card mx-auto mt-5" style="width: 18rem;">
-    //         <div class="card-body justify-content-center">
-    //             <h5 class="card-title"></h5>
-    //             <h6 class="card-subtitle mb-2 text-muted"></h6>
-    //             <p class="card-text"></p>
-    //         </div>
-    //     </div>
+   
+    
     //     <div class="card mx-auto mt-5" style="width: 18rem;">
     //         <div class="card-body justify-content-center">
     //             <h5 class="card-title"></h5>
@@ -55,8 +72,7 @@ class Ui {
     //     </div>
     
         
-    // </div>
-    // </div>
+  
     // `
     // }
 }
